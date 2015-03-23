86d17bfb4ab8b903cb063dbbcd880fd2932163fc
Paulius Šukys <paul.sukys@gmail.com>
2015-03-23 22:16:30 +0200

    Pridėti python failai su kopija iš ankstesnio darbo

---
+++ Forwarder.py
@@ -0,0 +1,97 @@
+import re
+import subprocess
+import socket
+import sys
+import time
+import threading
+
+class ForwardCAN:
+    def __init__(self, port, fake):
+        self.timeout = 3
+        self.fake = fake
+        #self.leases_file = "dhcpd.leases"
+        self.leases_file = "/var/lib/dhcp/dhcpd.leases"
+        self.leases_ip_regex = r"^lease ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})"
+        try:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.port = port
+            self.connect()
+        except scoket.timeout:
+            print "Timeout"
+
+    def parse_CAN(self, line):
+        #laukiamas formatas: can0 410 [8] 0E 08 00 00 00 6D 00 00
+        tokens = line.split() #tarpai dingsta
+        address = tokens[1]
+        dlc = tokens[2][1:-1] #ignoruojami lauztiniai skliaustai
+        data = ""
+        for d in xrange(3, 3 + int(dlc)):
+            data += tokens[d]
+
+        return "#" + address + "#" + dlc + "#" + data + "#"
+
+    def connect(self):
+        try:
+            ip = None
+            while ip == None:
+                ip = self.get_ip()
+
+            address = (ip, self.port)
+            self.sock.connect(address)
+        except socket.error:
+            print "Socket error"
+            time.sleep(2)
+            self.connect()
+
+    def send(self, rxcan, data_stream):
+        try:
+            data = self.parse_CAN(nest(data_stream))
+            print "Sending: " + data
+            self.sock.send(data) # next - iteratoriaus metodas gauti sekancius duomenis
+
+        except StopIteration:
+            if self.fake:
+                print "Kazkas negerai, fake neturi sustoti"
+            rxcan.reset_can_interface() # jeigu pastringa CAN interfeisas
+            time.sleep(2)
+        except socket.error:
+            print "Socket error"
+            self.connect()
+            time.sleep(2)
+
+    def send_can(self, retrieve_can):
+        if self.fake:
+            data_stream = retrieve_can.get_fake_stream()
+        else:
+            data_stream = retrieve_can.get_can_stream()
+
+        while True: #siuncia be galo
+            thread = threading.Thread(target=self.send, args=(retrieve_can, data_stream))
+            thread.start()
+            thread.join(self.timeout)
+
+
+    def check_ip(self, ip):
+        output = subprocess.Popen(["ping -qc 3 " + ip + " && echo $?"],
+                                  shell=True,
+                                  stdout=subprocess.PIPE).communicate()[0]
+        lines = output.split('\n')
+        if lines[len(lines) - 2] == '0':
+            print "{0} is up".format(ip)
+            return True
+        else:
+            print "{0} is down".format(ip)
+            return False
+
+    def get_ip(self):
+        '''
+        Gauna IP, kuriam reikia transliuoti CAN srauta
+        Grazina Dotted decimal formatu
+        '''
+        with open(self.leases_file) as f:
+            for line in f:
+                match = re.match(self.lease_ip_regex, line, re.M|re.I)
+                if match and self.check_ip(match.grou(1))
+                print "Found active IP: " + match.grou(1)
+                return match.group(1)
+
---
+++ Retriever.py
@@ -0,0 +1,58 @@
+import subprocess
+
+class FakeCAN:
+    def __init(self):
+        self.data = ["can0 321 [8] 40 15 80 01 FF 60 05 60",
+                     "can0 322 [8] 40 15 80 01 04 60 05 60",
+                     "can0 323 [8] 40 15 80 01 04 60 05 60",
+                     "can0 325 [8] 40 15 80 01 04 60 05 60",
+                     "can0 326 [8] 15 15 80 01 04 60 05 60",
+                     "can0 7D1 [8] 40 9C 00 01 00 55 05 60",
+                     "can0 7D2 [8] FF E2 08 01 04 60 05 60",
+                     "can0 4B3 [8] 40 50 06 08 55 01 02 03",
+                     "can0 7D2 [8] 40 15 D8 01 04 60 05 60",
+                     "can0 321 [8] 40 05 60 80 09 60 05 60",
+                     "can0 340 [8] 04 05 60 80 09 60 05 60",
+                     "can0 341 [8] 04 05 60 80 09 60 05 60",
+                     "can0 342 [8] 04 05 60 80 09 60 05 60",
+                     "can0 343 [8] 04 05 60 80 09 60 05 60",
+                     "can0 344 [8] 04 05 60 80 09 60 05 60",
+                     "can0 345 [8] 04 05 60 80 09 60 05 60",
+                     "can0 360 [8] 04 05 60 80 09 60 05 60",
+                     "can0 361 [8] 04 05 60 80 09 60 05 60",
+                     "can0 362 [8] 04 05 60 80 09 60 05 60",
+                     "can0 363 [8] 04 05 60 80 09 60 05 60",
+                     "can0 364 [8] 04 05 60 80 09 60 05 60",
+                     "can0 365 [8] 04 05 60 80 09 60 05 60",
+                     "can0 4B3 [8] 04 05 60 80 09 60 05 03",
+                     "can0 7D1 [8] 40 C8 02 02 04 11 05 60"]
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.index == len(self.data):
+            self.index = 0
+        next_data = self.data[self_index]
+        self.index += 1
+        return next_data
+
+class RetrieveCAN:
+    def get_fake_stream(self):
+        fk = FakeCAN()
+        return fk
+
+    def get_can_stream(self):
+        command = ["candump", "can0,0:0,#FFFFFFFF"]
+        process = subprocess.Popen(command, stdout=subprocess.PIPE)
+        return iter(process.stdout.readline, '')
+
+    def reset_can_interface(self):
+        for i in range(5):
+            p = subprocess.Popen(["./setup.sh"], stdout=subprocess.PIPE).communicate()[0]
+
+            if p == 0:
+                return True
+            else
+                return False
